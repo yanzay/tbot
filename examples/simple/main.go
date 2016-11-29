a0a4dc32153b4176c763d9b2f0f941d0988a4b7b
Alexey Grachov <grachov.alexey@gmail.com>
2016-11-29 16:01:28 +0200

    Add example.

---
+++ examples/simple/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/yanzay/tbot"
+)
+
+func main() {
+	token := os.Getenv("TELEGRAM_TOKEN")
+	bot, err := tbot.NewServer(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Handle("yo", "YO!")
+
+	bot.HandleFunc("/hi", HiHandler)
+	bot.HandleFunc("/say {text}", SayHandler)
+	bot.HandleFunc("/sticker", StickerHandler)
+	bot.HandleFunc("/photo", PhotoHandler)
+
+	bot.HandleDefault(EchoHandler)
+
+	err = bot.ListenAndServe()
+	log.Fatal(err)
+}
+
+func HiHandler(message tbot.Message) {
+	message.Replyf("Hello, %s!", message.From.FirstName)
+	time.Sleep(1 * time.Second)
+	message.Reply("What's up?")
+}
+
+func SayHandler(message tbot.Message) {
+	message.Reply(message.Vars["text"])
+}
+
+func EchoHandler(message tbot.Message) {
+	message.Reply(message.Text)
+}
+
+func StickerHandler(message tbot.Message) {
+	message.ReplySticker("sticker.png")
+}
+
+func PhotoHandler(message tbot.Message) {
+	message.ReplyPhoto("photo.jpg", "it's me")
+}
