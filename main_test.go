d8fde5b56fd5634e742e08ab29d4536ad5879a02
Alexey Grachov <grachov.alexey@gmail.com>
2017-05-09 18:26:51 +0300

    Add functional tests template.

--- main_test.go
+++ main_test.go
@@ -18,14 +18,20 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
+var inputMessages = make(chan *adapter.Message)
+var outputMessages = make(chan *adapter.Message)
+
 type mockBot struct{}
 
-func (*mockBot) Send(*adapter.Message) error {
+func (*mockBot) Send(m *adapter.Message) error {
+	go func() {
+		outputMessages <- m
+	}()
 	return nil
 }
 
 func (*mockBot) GetUpdatesChan() (<-chan *adapter.Message, error) {
-	return make(chan *adapter.Message), nil
+	return inputMessages, nil
 }
 
 func (*mockBot) GetFileDirectURL(string) (string, error) {
@@ -38,4 +44,48 @@ func (*mockBot) GetUserName() string {
 
 func (*mockBot) GetFirstName() string {
 	return "TestFirstName"
+}
+
+func TestTextReply(t *testing.T) {
+	setup := func(s *Server) {
+		s.HandleFunc("/hi", func(m *Message) { m.Reply("hi") })
+	}
+	requestResponse(t, setup, "/hi", adapter.MessageText, "hi", adapter.MessageText)
+}
+
+func TestStickerReply(t *testing.T) {
+	setup := func(s *Server) {
+		s.HandleFunc("/sticker", func(m *Message) { m.ReplySticker("sticker.png") })
+	}
+	requestResponse(t, setup, "/sticker", adapter.MessageText, "sticker.png", adapter.MessageSticker)
+}
+
+func requestResponse(t *testing.T, setup func(*Server), inData string, inType adapter.MessageType, outData string, outType adapter.MessageType) {
+	inputMessages = make(chan *adapter.Message)
+	defer close(inputMessages)
+	outputMessages = make(chan *adapter.Message)
+	defer close(outputMessages)
+	s, err := NewServer("TEST:TOKEN")
+	if err != nil {
+		t.Errorf("Error creating server: %q", err)
+	}
+	setup(s)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			t.Errorf("Error listening and serving: %q", err)
+		}
+	}()
+	inputMessages <- &adapter.Message{
+		Data:    inData,
+		Type:    inType,
+		Replies: make(chan *adapter.Message),
+	}
+	out := <-outputMessages
+	if out.Type != outType {
+		t.Errorf("Output should be text")
+	}
+	if out.Data != outData {
+		t.Errorf("Output should be hi")
+	}
 }
