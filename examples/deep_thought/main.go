8de2ac24078cd780cf326fa5595e4cade303c3c0
Alexey Grachov <grachov.alexey@gmail.com>
2017-05-19 14:29:58 +0300

    Add deep thought example.

---
+++ examples/deep_thought/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/yanzay/tbot"
+)
+
+func main() {
+	bot, err := tbot.NewServer(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	bot.Handle("/answer", "42")
+	bot.HandleFunc("/timer {seconds}", timerHandler)
+	bot.ListenAndServe()
+}
+
+func timerHandler(m *tbot.Message) {
+	// m.Vars contains all variables, parsed during routing
+	secondsStr := m.Vars["seconds"]
+	// Convert string variable to integer seconds value
+	seconds, err := strconv.Atoi(secondsStr)
+	if err != nil {
+		m.Reply("Invalid number of seconds")
+		return
+	}
+	m.Replyf("Timer for %d seconds started", seconds)
+	time.Sleep(time.Duration(seconds) * time.Second)
+	m.Reply("Time out!")
+}
