f506be4648c77a1d6a8aaeb55e40f232cc5fbfcf
Konstantin Kondr <kondr179@rambler.ru>
2017-07-20 21:18:23 +0300

    fix adapter

--- model/message.go
+++ model/message.go
@@ -23,3 +23,12 @@ type Message struct {
 	Buttons         [][]string
 	OneTimeKeyboard bool
 }
+
+// User is a user on Telegram.
+type User struct {
+	ID           int    `json:"id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`     // optional
+	UserName     string `json:"username"`      // optional
+	LanguageCode string `json:"language_code"` // optional
+}
