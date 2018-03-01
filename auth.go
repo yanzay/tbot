5a9df33435b901a8e2e7c4db91512656fe7ad9d3
Oğuzhan Kaymak <34816459+iowar@users.noreply.github.com>
2018-03-01 07:43:24 +0100

    Added userid and chatid based auth with example usage (#27)
    * support userid and chatid auth
    
    * add auth example
    
    * remove print
    
    * simple corrections
    
    * add UserId to model.Message
    
    * update auth_test

--- auth.go
+++ auth.go
@@ -1,20 +1,81 @@
 package tbot
 
-// NewAuth creates Middleware for white-list based authentication
-func NewAuth(whitelist []string) Middleware {
+import (
+	"errors"
+)
+
+const (
+	username = "Username"
+	userid   = "Userid"
+	chatid   = "Chatid"
+)
+
+//NewAuth creates Middleware for white-list based
+//authentication according to username, userid or chatid list.
+//purpose: to prevent the access to bots from another users or groups
+func NewAuth(whitelist interface{}) Middleware {
+	switch whitelist.(type) {
+	case []string:
+		return authWithUserName(whitelist.([]string))
+	case []int:
+		return authWithUserId(whitelist.([]int))
+	case []int64:
+		return authWithChatId(whitelist.([]int64))
+	default:
+		panic(errors.New("Unknown Whitelist Format"))
+	}
+}
+
+func authWithUserName(whitelist []string) Middleware {
 	return func(f HandlerFunction) HandlerFunction {
 		return func(m *Message) {
-			for _, name := range whitelist {
-				if m.From.UserName == name {
+			for _, userName := range whitelist {
+				if m.From.UserName == userName {
 					f(m)
 					return
 				}
 			}
-			accessDenied(m)
+			accessDenied(m, username)
 		}
 	}
 }
 
-func accessDenied(m *Message) {
-	m.Replyf("Access denied for user %s", m.From.UserName)
+func authWithUserId(whitelist []int) Middleware {
+	return func(f HandlerFunction) HandlerFunction {
+		return func(m *Message) {
+			for _, userId := range whitelist {
+				if m.From.ID == userId {
+					f(m)
+					return
+				}
+			}
+			accessDenied(m, userid)
+		}
+	}
+}
+
+func authWithChatId(whitelist []int64) Middleware {
+	return func(f HandlerFunction) HandlerFunction {
+		return func(m *Message) {
+			for _, chatId := range whitelist {
+				if m.ChatID == chatId {
+					f(m)
+					return
+				}
+			}
+			accessDenied(m, chatid)
+		}
+	}
+}
+
+func accessDenied(m *Message, opt string) {
+	switch opt {
+	case username:
+		m.Replyf("Access Denied For This %s: %s", opt, m.From.UserName)
+	case userid:
+		m.Replyf("Access Denied For This %s: %d", opt, m.From.ID)
+	case chatid:
+		m.Replyf("Access Denied For This %s: %d", opt, m.ChatID)
+	default:
+	}
 }
--- auth_test.go
+++ auth_test.go
@@ -1,17 +1,19 @@
 package tbot
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAccessDenied(t *testing.T) {
 	message := mockMessage()
-	go accessDenied(message)
+	go accessDenied(message, "Userid")
 	reply := <-message.Replies
 	if reply == nil {
 		t.Fail()
 	}
 }
 
-func TestAuthSuccess(t *testing.T) {
+func TestAuthWithUserNameSuccess(t *testing.T) {
 	auth := NewAuth([]string{"me"})
 	message := mockMessage()
 	invoked := false
@@ -22,8 +24,54 @@ func TestAuthSuccess(t *testing.T) {
 	}
 }
 
-func TestAuthFail(t *testing.T) {
+func TestAuthWithUserIdSuccess(t *testing.T) {
+	auth := NewAuth([]int{521844285})
+	message := mockMessage()
+	invoked := false
+	handler := func(m *Message) { invoked = true }
+	auth(handler)(message)
+	if !invoked {
+		t.Fail()
+	}
+}
+
+func TestAuthWithChatIdSuccess(t *testing.T) {
+	auth := NewAuth([]int64{-271651159})
+	message := mockMessage()
+	invoked := false
+	handler := func(m *Message) { invoked = true }
+	auth(handler)(message)
+	if !invoked {
+		t.Fail()
+	}
+}
+
+func TestAuthUserNameFail(t *testing.T) {
 	auth := NewAuth([]string{"notme"})
+	message := mockMessage()
+	invoked := false
+	handler := func(m *Message) { invoked = true }
+	go auth(handler)(message)
+	<-message.Replies
+	if invoked {
+		t.Fail()
+	}
+}
+
+func TestAuthUserIdFail(t *testing.T) {
+	auth := NewAuth([]int{1234567})
+	message := mockMessage()
+	invoked := false
+	handler := func(m *Message) { invoked = true }
+	go auth(handler)(message)
+	<-message.Replies
+	if invoked {
+		t.Fail()
+	}
+}
+
+func TestAuthChatIdFail(t *testing.T) {
+	auth := NewAuth([]int64{11235813})
 	message := mockMessage()
 	invoked := false
 	handler := func(m *Message) { invoked = true }
