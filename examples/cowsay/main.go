2c517b819045c370e59589c095c1fc09c6d7289c
Alexey Grachov <grachov.alexey@gmail.com>
2019-03-30 16:58:45 +0200

    Add cowsay example.

---
+++ examples/cowsay/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/yanzay/tbot"
+)
+
+func main() {
+	token := os.Getenv("TELEGRAM_TOKEN")
+	bot := tbot.New(token)
+	c := bot.Client()
+	bot.HandleMessage("cowsay .+", func(m *tbot.Message) {
+		text := strings.TrimPrefix(m.Text, "cowsay ")
+		cow := fmt.Sprintf("```\n%s\n```", cowsay(text))
+		c.SendMessage(m.Chat.ID, cow, tbot.OptParseModeMarkdown)
+	})
+	bot.Start()
+}
+
+func cowsay(text string) string {
+	lineLen := utf8.RuneCountInString(text) + 2
+	topLine := fmt.Sprintf(" %s ", strings.Repeat("_", lineLen))
+	textLine := fmt.Sprintf("< %s >", text)
+	bottomLine := fmt.Sprintf(" %s ", strings.Repeat("-", lineLen))
+	cow := `
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+               ||----w |
+               ||     ||
+	`
+	resp := fmt.Sprintf("%s\n%s\n%s%s", topLine, textLine, bottomLine, cow)
+	return resp
+}
