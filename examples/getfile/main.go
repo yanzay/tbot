4c7df913c6d4c71be2b7b488e565a31576869362
Alexey Grachov <grachov.alexey@gmail.com>
2019-11-16 13:43:17 +0200

    Add an example how to download file from message.

---
+++ examples/getfile/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/yanzay/tbot/v2"
+)
+
+func main() {
+	token := os.Getenv("TELEGRAM_TOKEN")
+	bot := tbot.New(token)
+	client := bot.Client()
+	bot.HandleMessage("", func(m *tbot.Message) {
+		// here we check if message contains Document
+		// you could also check for other types of files:
+		// Audio, Photo, Video, etc.
+		if m.Document != nil {
+			doc, err := client.GetFile(m.Document.FileID)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			url := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", token, doc.FilePath)
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+			out, err := os.Create(m.Document.FileName)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer out.Close()
+			io.Copy(out, resp.Body)
+		}
+	})
+	log.Fatal(bot.Start())
+}
